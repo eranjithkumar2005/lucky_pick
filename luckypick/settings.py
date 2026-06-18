b06cf810991974d23f6849677dd7ece56d71e9bc
E.Ranjithkumar <144881053+eranjithkumar2005@users.noreply.github.com>
2026-06-18 11:13:10 +0530

    Fix ALLOWED_HOSTS

--- luckypick/settings.py
+++ luckypick/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR /'staticfiles'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = 'static/',
+STATIC_ROOT = BASE_DIR /'staticfiles',
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField',
+STATICFILES_DIRS=[BASE_DIR / "myapp/static",
+]
